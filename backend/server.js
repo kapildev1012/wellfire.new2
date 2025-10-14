6a0a35d0def8f16c486bd8bbd8a63136268f7e5a
kapildev1012 <kapil16072004@gmail.com>
2025-10-14 20:46:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ connectCloudinary();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "http://localhost:5180", "http://localhost:3000", "https://your-frontend-domain.com"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:5176", "http://localhost:5180", "http://localhost:3000", "https://wellfire-frontend.onrender.com"],
     credentials: true
 }));
 
